c11470054e8510b05fb4dcddfda77cd7960e1830
Nete <null>
2024-09-22 03:18:47 -0700

    The single fundraisers page complete

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const PORT = 3000;
 
 // the server
 const server = http.createServer((req, res) => {
-  
-        const filePath = path.join(__dirname, 'pages', 'index.html');
+    const url = req.url;
+
+    if (url.startsWith('/fundraisers/')) {
+        const id = url.split('/')[2]; // Extracting the ID from the URL
+        const filePath = path.join(__dirname, 'pages', 'fundraiser.html'); 
         
-        // Reading the HTML file
+        // Reading the HTML file for a single fundraiser
         fs.readFile(filePath, (err, content) => {
             if (err) {
                 res.writeHead(500, { 'Content-Type': 'text/plain' });
@@ -19,7 +22,20 @@ const server = http.createServer((req, res) => {
                 res.end(content);
             }
         });
- 
+    } else {
+        const filePath = path.join(__dirname, 'pages', 'index.html');
+        
+        // Reading the HTML file for the main page
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        });
+    }
 });
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
